fd3afcf38b4c7e7f77df7feb58e6aa9f66f77e72
Rabz92 <rabbiailyas92@gmail.com>
2021-06-28 14:03:13 +0200

    header footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import { BrowserRouter,Route } from 'react-router-dom'
+import Home from './components/Home';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import Details from './components/Details';
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
-  );
+    <BrowserRouter>
+      <NavBar/>
+    
+      <Route exact path="/" render={(routerProps)=> <Home {...routerProps} />} />
+
+      <Route exact path="/details/:id" render={(routerProps)=> <Details {...routerProps} />}/>
+
+      <Footer/>
+    </BrowserRouter>
+  )
 }
 
 export default App;
